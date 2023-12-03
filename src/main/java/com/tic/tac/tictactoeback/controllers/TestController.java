55a36e0f74d16ccd7dd325e629944739b5d1932d
Kacper Mejsner <kacpermejs@gmail.com>
2023-12-03 20:27:03 +0100

    Dockerized; /test rest controller

---
+++ src/main/java/com/tic/tac/tictactoeback/controllers/TestController.java
@@ -0,0 +1,19 @@
+package com.tic.tac.tictactoeback.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/")
+    String testConnection() {
+        return "Success!";
+    }
+}
