09ce06d83918fe9f30755b6ac9d5ad8ba80cd392
Kacper Mejsner <kacpermejs@gmail.com>
2023-12-10 14:12:13 +0100

    no secrets for now

--- src/main/java/com/tic/tac/tictactoeback/config/ExampleSecret.java
+++
@@ -1,8 +0,0 @@
-package com.tic.tac.tictactoeback.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("secrets")
-public record ExampleSecret(String SomeSecretApiKey) {
-
-}
--- src/main/java/com/tic/tac/tictactoeback/controllers/TestController.java
+++ src/main/java/com/tic/tac/tictactoeback/controllers/TestController.java
@@ -1,7 +1,5 @@
 package com.tic.tac.tictactoeback.controllers;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
